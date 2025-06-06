689a57e7734b0bb39672fed1a1a9aa1c1b285463
Lucas <lukaskaua172003@gmail.com>
2025-06-06 06:45:55 -0300

    refactor: iniciar view pelo findViewById

--- app/src/main/java/com/example/faturamentoapp/MainActivity.java
+++ app/src/main/java/com/example/faturamentoapp/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.faturamentoapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,12 +15,25 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private NumberPicker mNumberPickerAno;
+    private RadioGroup mRadioGroupAdicionaExclui;
+    private EditText mEditTextValor;
+    private TextView mTextViewSaldo;
+    private Button mButtonConfirma;
+    private Button mButtonAdicionarTitulo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        mNumberPickerAno = findViewById(R.id.npAno);
+        mRadioGroupAdicionaExclui = findViewById(R.id.rgAdicionaExclui);
+        mEditTextValor = findViewById(R.id.etValor);
+        mTextViewSaldo = findViewById(R.id.tvSaldo);
+        mButtonConfirma = findViewById(R.id.btnConfirma);
+        mButtonAdicionarTitulo = findViewById(R.id.btnAdicionarTitulo);
 
     }
 }
