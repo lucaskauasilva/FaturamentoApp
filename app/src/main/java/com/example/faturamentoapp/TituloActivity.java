938f9eb947add6f6633aced7ed0f6d21f4d9fe0f
Lucas <lukaskaua172003@gmail.com>
2025-06-06 15:40:42 -0300

    feat: inicializar EditText e Button na activity_titulo utilizando findViewById e prepará-los para implementação futura

--- app/src/main/java/com/example/faturamentoapp/TituloActivity.java
+++ app/src/main/java/com/example/faturamentoapp/TituloActivity.java
@@ -1,6 +1,8 @@
 package com.example.faturamentoapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,20 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class TituloActivity extends AppCompatActivity {
 
+    private EditText mEditTextNomeFantasia;
+    private Button mButtonCadastrar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_titulo);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        mEditTextNomeFantasia = findViewById(R.id.etNomeFantasia);
+        mButtonCadastrar = findViewById(R.id.btnCadastrar);
+
+        /* @+id/etNomeFantasia (mEditTextNomeFantasia) */ //(à implementar)
+
+        /* @+id/btnCadastrar (mButtonCadastrar) */ //(à implementar)
     }
 }
